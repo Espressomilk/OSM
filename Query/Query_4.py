a3918ca08ca5f928c455650ef37ac927504d1c8b
hongruz <hongruz95@gmail.com>
2016-11-27 17:09:33 +0800

    Add files via upload

--- Query/Query_4.py
+++ Query/Query_4.py
@@ -28,8 +28,36 @@ def Query4ByLLR(lat,lon,rad):
     cur.execute("set @poly='Polygon((%f %f,%f %f,%f %f,%f %f,%f %f))'" %(x-rad,y+rad,x+rad,y+rad,x+rad,y-rad,x-rad,y-rad,x-rad,y+rad))
     cur.execute('select * from POIs where MBRContains(ST_GeomFromText(@poly),g)')
     queryResult = cur.fetchall()
-    return queryResult
+    ans = []
+    for row in queryResult:
+        #x = dist
+        if x <=rad:
+            ans.append(queryResult)
+    return ans
 
+def Query5ByLL(lat,lon):
+    (x,y) = ut.mapping(lat,lon)
+    rad = 10
+    queryResult = []
+    while True:
+        cur.execute("set @poly='Polygon((%f %f,%f %f,%f %f,%f %f,%f %f))'" %(x-rad,y+rad,x+rad,y+rad,x+rad,y-rad,x-rad,y-rad,x-rad,y+rad))
+        cur.execute('')
+        queryResult = cur.fetchall()
+        if len(queryResult)>0:
+            break
+        else:
+            rad = rad*2.7
+    ans = 0
+    minx = 1e8
+    for row in queryResult:
+        #x = dist
+        if x < minx:
+            minx = x
+            ans = row
+    return ans
+
+def Query6By2LL(lat1,lon1,lat2,lon2):
+    return 
 
 if __name__ == "__main__":
     print(Query1ByNodeID(28111460))
